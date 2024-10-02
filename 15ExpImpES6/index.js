f1aacece9200735cd4500b1ab2f61b9c206e6a4a
Dinesh Magar <magard1010@gmail.com>
2024-10-03 03:14:23 +0545

    import export ES6

---
+++ 15ExpImpES6/index.js
@@ -0,0 +1,10 @@
+// Import and Export module - ES6
+
+// import name from './student.js';
+// console.log(name)
+
+
+import {nm, marks} from './student.js';
+console.log(nm)
+
+marks(20, 30);
---
+++ 15ExpImpES6/student.js
@@ -0,0 +1,9 @@
+// Import and Export module - ES6
+const nm = "Ram";
+
+const marks = (math, sci) => {
+    console.log(math + sci);
+}
+
+// export default nm;
+export {nm, marks};
