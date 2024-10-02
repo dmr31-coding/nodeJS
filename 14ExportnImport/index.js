94549e582893c16f04d2d6fa9a725a89e8d5772a
Dinesh Magar <magard1010@gmail.com>
2024-10-03 03:13:00 +0545

    export import commonJS

---
+++ 14ExportnImport/index.js
@@ -0,0 +1,12 @@
+// Import and Export module - Common JS
+//export import garda var name diff. rakhda pani  kam garxa xuttai file ma same file maa ta eutain name hunuparyo without {}
+//with {} same name for var
+
+// const name = require('./student.js');
+// console.log(name)
+
+
+const {nm, marks} = require('./student.js');
+console.log(nm)
+
+marks(20, 30);
---
+++ 14ExportnImport/student.js
@@ -0,0 +1,9 @@
+// Import and Export module - Common JS
+
+const nm = "Ram";
+
+const marks = (math, sci) => {
+    console.log(math + sci);
+}
+
+module.exports = {nm, marks};
